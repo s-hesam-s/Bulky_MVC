027f097f0b3a2cffe6730da56a4a052aedb9545f
s-hesam-s <hesamshoushtarian@gmail.com>
2023-11-26 23:47:17 +0330

    Section 2 - Create Category Model And Data Annotations

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        //Key does not need if the name is Id Or CategoryId
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+    }   
+}
