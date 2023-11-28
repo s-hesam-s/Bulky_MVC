e4ae5f7c22d540909719b8037fe0ed5b1831e004
s-hesam-s <hesamshoushtarian@gmail.com>
2023-11-28 20:54:17 +0330

    Section 2 - Server Side Validations

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -11,9 +11,11 @@ namespace BulkyWeb.Models
 
         [Required]
         [DisplayName("Category Name")]
+        [MaxLength(50)]
         public string Name { get; set; }
 
         [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage = "Display Order must be between 1-100")]
         public int DisplayOrder { get; set; }
     }   
 }
