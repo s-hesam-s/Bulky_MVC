f0f058a233a5056cb63b5f555e9c4cd88d8e45f2
s-hesam-s <hesamshoushtarian@gmail.com>
2023-11-28 21:48:02 +0330

    Section 2 - Get Category Details to Edit

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -37,5 +37,35 @@ namespace BulkyWeb.Controllers
             }
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(x => x.Id == id);
+            //Category? categoryFromDb2 = _db.Categories.Where(x => x.Id == id).FirstOrDefault();
+
+            if (categoryFromDb == null)
+                return NotFound();
+            
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //    ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
