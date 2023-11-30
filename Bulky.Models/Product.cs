01e4a59ff2942ba0c3976282d12181e977eb22eb
s-hesam-s <hesamshoushtarian@gmail.com>
2023-11-30 21:33:37 +0330

    Section 6 - Add Foreign Key in EF Core

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
 namespace BulkyBook.Models
@@ -39,6 +40,10 @@ namespace BulkyBook.Models
         [Display(Name = "Price for 100+")]
         [Range(1, 1000)]
         public double Price100 { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
 
     }
 }
