ad123a8aa50f59e378891e849168b8567c549779
s-hesam-s <hesamshoushtarian@gmail.com>
2023-11-30 14:03:14 +0330

    Section 4 - N-Tier Architecture

--- Bulky.Models/Category.cs
+++ Bulky.Models/Category.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
-namespace BulkyWeb.Models
+namespace Bulky.Models
 {
     public class Category
     {
@@ -15,7 +15,7 @@ namespace BulkyWeb.Models
         public string Name { get; set; }
 
         [DisplayName("Display Order")]
-        [Range(1,100, ErrorMessage = "Display Order must be between 1-100")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
         public int DisplayOrder { get; set; }
-    }   
+    }
 }
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,4 +1,4 @@
-using BulkyWeb.Data;
+using Bulky.DataAccess;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
