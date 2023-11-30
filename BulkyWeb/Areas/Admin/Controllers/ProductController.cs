0c15bca0be176f89834f4e9915769300ed9494db
s-hesam-s <hesamshoushtarian@gmail.com>
2023-11-30 21:13:10 +0330

    Section 6 - Product CRUD Operations

---
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,100 @@
+﻿using BulkyBook.DataAccess;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            return View(objProductList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            Product? productFromDb = _unitOfWork.Product.Get(x => x.Id == id);
+            //Product? productFromDb1 = _db.Categories.FirstOrDefault(x => x.Id == id);
+            //Product? productFromDb2 = _db.Categories.Where(x => x.Id == id).FirstOrDefault();
+
+            if (productFromDb == null)
+                return NotFound();
+
+            return View(productFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            Product? productFromDb = _unitOfWork.Product.Get(x => x.Id == id);
+            //Product? productFromDb1 = _db.Categories.FirstOrDefault(x => x.Id == id);
+            //Product? productFromDb2 = _db.Categories.Where(x => x.Id == id).FirstOrDefault();
+
+            if (productFromDb == null)
+                return NotFound();
+
+            return View(productFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            Product? productFromDb = _unitOfWork.Product.Get(x => x.Id == id);
+            if (productFromDb == null)
+                return NotFound();
+
+            _unitOfWork.Product.Remove(productFromDb);
+            _unitOfWork.Save();
+            TempData["success"] = "Product deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
