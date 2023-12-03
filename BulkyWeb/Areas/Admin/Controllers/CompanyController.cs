87cc0572f3482e47b0cccf3707239a355f420216
s-hesam-s <hesamshoushtarian@gmail.com>
2023-12-04 00:23:28 +0330

    Section 9 - Company CRUD

---
+++ BulkyWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,87 @@
+﻿using BulkyBook.DataAccess;
+using BulkyBook.DataAccess.Repository;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<Company> companyList = _unitOfWork.Company.GetAll().ToList();
+
+            return View(companyList);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return View(new Company());
+            }
+            else
+            {
+                Company companyObj = _unitOfWork.Company.Get(x => x.Id == id);
+                return View(companyObj);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(Company companyObj)
+        {
+            if (ModelState.IsValid)
+            {
+                if (companyObj.Id == 0)
+                    _unitOfWork.Company.Add(companyObj);
+                else
+                    _unitOfWork.Company.Update(companyObj);
+
+                _unitOfWork.Save();
+                TempData["success"] = "Company created successfully";
+                return RedirectToAction("Index");
+            }
+            else                
+                return View(companyObj);
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Company> companyList = _unitOfWork.Company.GetAll().ToList();
+            return Json(new { data = companyList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            Company? companyToBeDeleted = _unitOfWork.Company.Get(x => x.Id == id);
+            if (companyToBeDeleted == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            _unitOfWork.Company.Remove(companyToBeDeleted);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+        #endregion
+    }
+}
