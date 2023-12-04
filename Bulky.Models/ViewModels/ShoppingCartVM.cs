042ae5b6e15bb5706e60d10d8fe129d1d038afb9
s-hesam-s <hesamshoushtarian@gmail.com>
2023-12-04 22:52:05 +0330

    Section 10 - Get order total in Shopping Cart

---
+++ Bulky.Models/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
+
+        public double OrderTotal { get; set; }
+    }
+}
--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,13 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BulkyBookWeb.Areas.Customer.Controllers
 {
+    [Area("Customer")]
+    [Authorize]
     public class CartController : Controller
     {
-        [Area("Customer")]
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            string userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(x =>
+                x.ApplicationUserId == userId, includeProperties: "Product")
+            };
+
+            foreach (ShoppingCart cart in ShoppingCartVM.ShoppingCartList)
+            {
+                cart.Price = GetPriceBasedOnQuantity(cart);
+                ShoppingCartVM.OrderTotal += (cart.Price * cart.Count);
+            }
+
+            return View(ShoppingCartVM);
+        }
+
+
+
+        private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
+        {
+            if (shoppingCart.Count <= 50)
+                return shoppingCart.Product.Price;
+            else
+            {
+                if (shoppingCart.Count <= 100)
+                    return shoppingCart.Product.Price50;
+                else
+                    return shoppingCart.Product.Price100;
+            }
         }
     }
 }
