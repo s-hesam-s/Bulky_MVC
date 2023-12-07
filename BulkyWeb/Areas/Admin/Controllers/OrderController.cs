c9a9b649d5df5b31a484ebc3de684cd003b8b90e
s-hesam-s <hesamshoushtarian@gmail.com>
2023-12-07 21:07:12 +0330

    Section 12 - Order List UI

--- BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class OrderController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
