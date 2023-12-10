6f1748f13f7ba769e851afa61cc8f546b292645a
s-hesam-s <hesamshoushtarian@gmail.com>
2023-12-10 22:16:11 +0330

    Section 15 - Display User List

---
+++ BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using BulkyBook.DataAccess;
+using BulkyBook.DataAccess.Repository;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> userList = _db.ApplicationUsers.Include(x => x.Company).ToList();
+            return Json(new { data = userList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+        #endregion
+    }
+}
